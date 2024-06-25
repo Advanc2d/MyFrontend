190216c8ee3cd58c5d71b71ff0e7f51dde656bcf
Advanc2d <phm940916@gmail.com>
2024-06-25 19:23:16 +0900

    🙈 feat: [MyFrontend] #comment 경로 오류 해결 - 24.06.25
    추신 : 맨앞에 붙일 수 있는 구분
    fix : Bug 류 처리 시 ( patch version up )
    feat : 신규 기능 류 처리 시 ( minor version up )
    perf : 메이저 기능 류 처리 시 ( major version up )
    
    #close #time 1h +review SR @Advanc2d
    
    [ Frontend::Advanc2d-MyFrontend::Advanc2d ]

--- phm/js/almServer.js
+++ phm/js/almServer.js
@@ -11,29 +11,32 @@ var dataTableRef; // 데이터테이블 참조 변수
 function execDocReady() {
 
 	var pluginGroups = [
-		["../reference/light-blue/lib/vendor/jquery.ui.widget.js",
-			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
-			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
-			"../reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
-			"../reference/light-blue/lib/jquery.iframe-transport.js",
-			"../reference/light-blue/lib/jquery.fileupload.js",
-			"../reference/light-blue/lib/jquery.fileupload-fp.js",
-			"../reference/light-blue/lib/jquery.fileupload-ui.js",
+		[
+		    "/src/reference/light-blue/lib/vendor/jquery.ui.widget.js",
+			"/src/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Templates_js_tmpl.js",
+			"/src/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Load-Image_js_load-image.js",
+			"/src/reference/light-blue/lib/vendor/http_blueimp.github.io_JavaScript-Canvas-to-Blob_js_canvas-to-blob.js",
+			"/src/reference/light-blue/lib/jquery.iframe-transport.js",
+			"/src/reference/light-blue/lib/jquery.fileupload.js",
+			"/src/reference/light-blue/lib/jquery.fileupload-fp.js",
+			"/src/reference/light-blue/lib/jquery.fileupload-ui.js",
 		],
 
-		["../reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
-			"../reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
-			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
-			"../reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
-			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
-			"../reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
-			"../reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"],
+		[
+		    "/src/reference/jquery-plugins/select2-4.0.2/dist/css/select2_lightblue4.css",
+			"/src/reference/jquery-plugins/lou-multi-select-0.9.12/css/multiselect-lightblue4.css",
+			"/src/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select-bluelight.css",
+			"/src/reference/jquery-plugins/select2-4.0.2/dist/js/select2.min.js",
+			"/src/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.quicksearch.js",
+			"/src/reference/jquery-plugins/lou-multi-select-0.9.12/js/jquery.multi-select.js",
+			"/src/reference/jquery-plugins/multiple-select-1.5.2/dist/multiple-select.min.js"],
 
-		["../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
-			"../reference/light-blue/lib/bootstrap-datepicker.js",
-			"../reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
-			"../reference/lightblue4/docs/lib/widgster/widgster.js",
-			"../reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
+		[
+		    "/src/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.min.css",
+			"/src/reference/light-blue/lib/bootstrap-datepicker.js",
+			"/src/reference/jquery-plugins/datetimepicker-2.5.20/build/jquery.datetimepicker.full.min.js",
+			"/src/reference/lightblue4/docs/lib/widgster/widgster.js",
+			"/src/reference/lightblue4/docs/lib/slimScroll/jquery.slimscroll.min.js",
 			"./html/sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js",
 			"./html/sb-admin/vendor/datatables/dataTables.bootstrap4.min.css",
 			"./html/sb-admin/vendor/datatables/jquery.dataTables.min.js",
@@ -42,19 +45,20 @@ function execDocReady() {
 		],
 
 		[
-			/*"../reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",*/
-			"../reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
-			"../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"]
-		// 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
+			/*"/src/reference/jquery-plugins/dataTables-1.10.16/media/css/jquery.dataTables_lightblue4.css",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/css/responsive.dataTables_lightblue4.css",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Select/css/select.dataTables_lightblue4.css",*/
+			"/src/reference/jquery-plugins/dataTables-1.10.16/media/js/jquery.dataTables.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Responsive/js/dataTables.responsive.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Select/js/dataTables.select.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/RowGroup/js/dataTables.rowsGroup.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/dataTables.buttons.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.html5.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/buttons.print.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/jszip.min.js",
+			"/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/pdfmake.min.js"
+			// 추가적인 플러그인 그룹들을 이곳에 추가하면 됩니다.
+        ]
 	];
 
 	loadPluginGroupsParallelAndSequential(pluginGroups)
@@ -67,7 +71,7 @@ function execDocReady() {
 			//vfs_fonts 파일이 커서 defer 처리 함.
 			setTimeout(function () {
 				var script = document.createElement("script");
-				script.src = "../reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/vfs_fonts.js";
+				script.src = "/src/reference/jquery-plugins/dataTables-1.10.16/extensions/Buttons/js/vfs_fonts.js";
 				script.defer = true; // defer 속성 설정
 				document.head.appendChild(script);
 			}, 3000); // 3초 후에 실행됩니다.
